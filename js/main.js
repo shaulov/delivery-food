df14e6811b437c3a3cb93ed169e073cca7a6b65b
shaulov <32754041+shaulov@users.noreply.github.com>
2020-05-07 19:44:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,176 @@
+"use strict";
+
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
+
+let login = localStorage.getItem('userLogin');
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+function toggleModalAuth() {
+    modalAuth.classList.toggle('is-open');
+}
+
+function authorized() {
+
+    function logOut() {
+        login = null;
+        localStorage.removeItem('userLogin');
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        buttonOut.removeEventListener('click', logOut);
+        checkAuth();
+    }
+
+    userName.textContent = login;
+
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+
+    buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthoeized() {
+
+    function logIn(event) {
+        event.preventDefault();
+        login = loginInput.value;
+
+        if (!login || login.length < 4 || login == ' ') {
+            alert('Некорректный логин');
+        } else {
+            localStorage.setItem('userLogin', login);
+
+            toggleModalAuth();
+            buttonAuth.removeEventListener('click', toggleModalAuth);
+            closeAuth.removeEventListener('click', toggleModalAuth);
+            logInForm.removeEventListener('submit', logIn);
+            logInForm.reset();
+            checkAuth();
+        }
+    }
+
+    buttonAuth.addEventListener('click', toggleModalAuth);
+    closeAuth.addEventListener('click', toggleModalAuth);
+    logInForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+    if (login) {
+        authorized();
+    } else {
+        notAuthoeized();
+    }
+}
+
+function createCardRestaurant() {
+    const card = `
+        <a class="card card-restaurant">
+            <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">Пицца плюс</h3>
+                    <span class="card-tag tag">50 мин</span>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="rating">
+                        4.5
+                    </div>
+                    <div class="price">От 900 ₽</div>
+                    <div class="category">Пицца</div>
+                </div>
+            </div>
+        </a>
+    `;
+
+    cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+
+function createCardGood() {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    card.insertAdjacentHTML('beforeend', `
+            <img src="img/pizza-plus/pizza-girls.jpg" alt="image" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">Пицца Девичник</h3>
+                </div>
+                <div class="card-info">
+                    <div class="ingredients">Соус томатный, постное тесто, нежирный сыр, кукуруза, лук,
+                        маслины,
+                        грибы, помидоры, болгарский перец.
+                    </div>
+                </div>
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">450 ₽</strong>
+                </div>
+            </div>
+    `);
+
+    cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+function openGoods(event) {
+
+    if (login) {
+        const target = event.target;
+        const restaurant = target.closest('.card-restaurant');
+
+        if (restaurant) {
+            containerPromo.classList.add('hide');
+            restaurants.classList.add('hide');
+            menu.classList.remove('hide');
+
+            cardsMenu.textContent = '';
+
+            createCardGood();
+            createCardGood();
+            createCardGood();
+        }
+    } else {
+        toggleModalAuth();
+    }
+}
+
+
+cartButton.addEventListener("click", toggleModal);
+
+close.addEventListener("click", toggleModal);
+
+cardsRestaurants.addEventListener('click', openGoods);
+
+logo.addEventListener('click', function () {
+    containerPromo.classList.remove('hide');
+    restaurants.classList.remove('hide');
+    menu.classList.add('hide');
+});
+
+checkAuth();
+
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
